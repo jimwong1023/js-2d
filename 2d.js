7eb661c43a14e25e0e70569e787462b0ca96fd49
Jimmy <jimwong1023@gmail.com>
2014-07-30 15:25:28 -0700

    Create grid

--- 2d.js
+++ 2d.js
@@ -6,3 +6,21 @@ function Vector(x, y) {
 Vector.prototype.plus = function (other) {
 	return new Vector(this.x + other.x, this.y + other.y);
 };
+
+function Grid (width, height) {
+	this.space = new Array(width * height);
+	this.width = width;
+	this.height = height;
+}
+
+Grid.prototype.isInside = function (vector) {
+	return vector.x >= 0 && vector.x < this.width && vector.y >= 0 && vector.y < this.height;
+};
+
+Grid.protoype.get = function (vector) {
+	return this.space[vector.x + this.width * vector.y];
+};
+
+Grid.prototype.set = function (vector, value) {
+	this.space[vector.x + this.width * vector.y] = value;
+};
