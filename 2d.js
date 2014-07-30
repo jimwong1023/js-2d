9e6dd1013ed5b96b839d7acda26dcb4a7270d776
Jimmy <jimwong1023@gmail.com>
2014-07-30 15:57:12 -0700

    Create World

--- 2d.js
+++ 2d.js
@@ -17,10 +17,54 @@ Grid.prototype.isInside = function (vector) {
 	return vector.x >= 0 && vector.x < this.width && vector.y >= 0 && vector.y < this.height;
 };
 
-Grid.protoype.get = function (vector) {
+Grid.prototype.get = function (vector) {
 	return this.space[vector.x + this.width * vector.y];
 };
 
 Grid.prototype.set = function (vector, value) {
 	this.space[vector.x + this.width * vector.y] = value;
 };
+
+function elementFromChar (legend, ch) {
+  if (ch == " ")
+    return null;
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+function charFromElement (element) {
+	if (element === null) {
+		return " ";
+	}
+	else {
+		return element.originChar;
+	}
+}
+
+function World (map, legend) {
+	var grid = new Grid(map[0].length, map.length);
+	this.grid = grid;
+	this.legend = legend;
+
+	map.forEach(function (line, y) {
+		for (var x = 0; x < line.length; x++) {
+			grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
+		}
+	});
+}
+
+World.prototype.toString = function () {
+	var output = "";
+	for (var y = 0; y < this.grid.height; y++) {
+		for (var x = 0; x < this.grid.width; x++) {
+			var element = this.grid.get(new Vector(x,y));
+			output += charFromElement(element);
+		}
+		output += "\n";
+	}
+	return output;
+}
+
+
+
